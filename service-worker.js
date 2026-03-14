ecc5fd66257531c04dbd80f8f9d7c0732c704f4c
brocosalesretail-svg <brocosalesretail@gmail.com>
2026-03-14 10:22:50 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,11 @@ const CACHE_NAME = 'si-tukang-v1';
 const ASSETS = [
   './',
   './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://unpkg.com/html2pdf.js@0.10.1/dist/html2pdf.bundle.min.js',
+  'https://unpkg.com/@supabase/supabase-js@2',
   'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
